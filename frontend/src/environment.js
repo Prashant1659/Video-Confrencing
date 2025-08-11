fe28d20a012f0cbbb3e996446b6b44e44a582abb
Prashant <singhkaushal9561@gmail.com>
2025-08-11 23:38:45 +0530

    Ready for deployment

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://video-confrencing-9541.onrender.com" :
 
     "http://localhost:8000"
 
--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -12,7 +12,8 @@ import StopScreenShareIcon from '@mui/icons-material/StopScreenShare';
 import ChatIcon from '@mui/icons-material/Chat';
 import {useNavigate} from 'react-router';
 import io from 'socket.io-client';
-const server_url = 'http://localhost:8000';
+import server from '../environment';
+const server_url = server;
 
 var connections = {};
 
